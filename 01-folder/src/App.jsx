99fc37828bcceffe80126aaddc68b7195fa4ea87
Tanmay Chopra <tanmay.chopra2514@gmail.com>
2026-01-02 17:31:11 +0530

    Initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> 
+    <div id="parent">
+        <h1 id='child1'>Hello World</h1>
+        <h2 id='child2'>Hello World</h2>
+    </div>
+    <div id='chacha'></div>
+    </>
+  )
+}
+
+export default App
