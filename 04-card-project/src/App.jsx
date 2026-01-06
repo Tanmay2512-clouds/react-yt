1545493d3d91f1f0d348716c66265203d6c882d5
Tanmay Chopra <tanmay.chopra2514@gmail.com>
2026-01-06 20:34:33 +0530

    made some changes

--- 04-card-project/src/App.jsx
+++ 04-card-project/src/App.jsx
@@ -7,13 +7,24 @@ const App = () => {
     <div className='Parent'>
       <div className="card">
         <div className="top">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png" alt="" srcset="" />
+          <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png" alt="" srcset="" />
           <button>Save <Bookmark /></button>
         </div>
         <div className="center">
+          <h3>Amazon <span>5 days ago</span></h3>
+          <h2>Senior UI/Ux designer</h2>
+          <div className="center2">
+            <h4 className="c21">Part-time</h4>
+            <h4 className="c22">Senior level</h4>
+          </div>
 
         </div>
         <div className="bottom">
+          <div>
+            <h3>$120/HR</h3>
+            <p>Mumbai, India</p>
+            <button>Apply Now</button>
+          </div>
 
         </div>
       </div>
