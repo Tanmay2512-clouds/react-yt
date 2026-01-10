7b73cb90ac8b92e821852c380c92c146032770fb
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-01-10 14:14:28 +0530

    some ways of writing css

---
+++ 05-tailwind/src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/button.css'
+
+const Button = () => {
+  return (
+    <div style={styles.btn}> 
+        This is a button
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-tailwind/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/button.css'
+const Header = () => {
+  return (
+    <div style={styles.header}>
+        Tanmay <button style={styles.btn}>login</button>
+    </div>
+  )
+}
+
+export default Header
