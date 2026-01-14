f2cb58719360a5302fae4b24bef17f2d96524aac
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-01-14 16:53:01 +0530

    Learned how to use functions

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './index.css'
 
+function onscrollhandler(){
+  console.log('scrolling')
+}
+
 const App = () => {
   return (
-    <div>
-      
+    <div className='og' onWheel={onscrollhandler}>
+     <div className="page1"></div>
+     <div className="page2"></div>
+     <div className="page3"></div>
+     <div className="page4"></div>
+     <div className="page5"></div>
+     <div className="page6"></div>
+     <div className="page7"></div>
     </div>
   )
 }
